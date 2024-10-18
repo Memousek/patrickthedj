68de62d903b93bd6d9742748bc4dbaedf6ae764e
Memousek <namiskoczech@gmail.com>
2024-10-18 17:58:00 +0200

    XD

---
+++ config.js
@@ -0,0 +1,46 @@
+require("dotenv").config();
+
+module.exports = {
+  app: {
+    token: process.env.TOKEN,
+    playing: "/play | DEV Mode 👽",
+    global: true,
+    guild: "1239135898612924438",
+    extraMessages: false,
+    loopMessage: false,
+    lang: "cs",
+    enableEmojis: true,
+  },
+
+  emojis: {
+    back: "⏪",
+    skip: "⏩",
+    ResumePause: "⏯️",
+    savetrack: "💾",
+    volumeUp: "🔊",
+    volumeDown: "🔉",
+    loop: "🔁",
+  },
+
+  opt: {
+    DJ: {
+      enabled: false,
+      roleName: "",
+      commands: [],
+    },
+    Translate_Timeout: 10000,
+    maxVol: 100,
+    spotifyBridge: true,
+    volume: 75,
+    leaveOnEmpty: true,
+    leaveOnEmptyCooldown: 30000,
+    leaveOnEnd: false,
+    leaveOnEndCooldown: 30000,
+    discordPlayer: {
+      ytdlOptions: {
+        quality: "highestaudio",
+        highWaterMark: 1 << 25,
+      },
+    },
+  },
+};
